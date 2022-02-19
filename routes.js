fb9f7323244181a31b22dad158026f6579aa5d92
Bernd Öggl <bernd@webman.at>
2022-02-19 11:31:30 +0000

    add eslint and fix style

--- routes.js
+++ routes.js
@@ -1,18 +1,20 @@
-const Jimp = require('jimp'),
-  express = require("express"),
-  multer = require('multer'),
-  upload = multer(),
-  debug = require("debug")("api"),
-  eparser = require('exif-parser'),
-  router = express.Router(),
-  uuid = require('uuid');
+const Jimp = require('jimp');
+const express = require('express');
+const multer = require('multer');
 
-router.get("/pictures", (req, res) => {
-  const rows = req.app.get("db").prepare("SELECT * from pic").all();
-  res.json({data: rows});
+const upload = multer();
+const debug = require('debug')('api');
+const eparser = require('exif-parser');
+
+const router = express.Router();
+const uuid = require('uuid');
+
+router.get('/pictures', (req, res) => {
+  const rows = req.app.get('db').prepare('SELECT * from pic').all();
+  res.json({ data: rows });
 });
-router.post("/picture", upload.single('file'), (req, res) => {
-  debug("Files uploaded: ", req.file);
+router.post('/picture', upload.single('file'), (req, res) => {
+  debug('Files uploaded: ', req.file);
   let origDate = Date.now();
   let hasExif = 0;
   if (req.file.mimetype === 'image/jpeg') {
@@ -25,21 +27,20 @@ router.post("/picture", upload.single('file'), (req, res) => {
       debug('exifdate: ', origDate);
     }
   }
-  Jimp.read(req.file.buffer).then(function (image) {
+  Jimp.read(req.file.buffer).then((image) => {
     image.cover(200, 200)
-      .getBuffer(req.file.mimetype, function (err, buff) {
-        const entry = [ uuid.v1(), origDate, Buffer.from(buff, 'base64'), req.file.mimetype, req.file.size, hasExif ];
-        const query = "INSERT INTO pic (id, date, thumbnail, mime, size, hasExif) VALUES (?,?,?,?,?,?)";
-        const data = req.app.get("db").prepare(query).run(entry);
-        res.json({ data: data});
-      })
-  })
+      .getBuffer(req.file.mimetype, (err, buff) => {
+        const entry = [uuid.v1(), origDate, Buffer.from(buff, 'base64'), req.file.mimetype, req.file.size, hasExif];
+        const query = 'INSERT INTO pic (id, date, thumbnail, mime, size, hasExif) VALUES (?,?,?,?,?,?)';
+        const data = req.app.get('db').prepare(query).run(entry);
+        res.json({ data });
+      });
+  });
 });
-router.delete("/picture/:id", (req, res) => {
+router.delete('/picture/:id', (req, res) => {
   debug('delete for ', req.params.id);
-  const data = req.app.get("db").prepare("DELETE FROM pic WHERE id = ?").run([req.params.id]);
-  res.json({data: data});
+  const data = req.app.get('db').prepare('DELETE FROM pic WHERE id = ?').run([req.params.id]);
+  res.json({ data });
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // Datei: npm-security/server.js
-const express = require("express"),
-  routes = require('./routes'),
-  sqlite = require('better-sqlite3'),
-  path = require('path'),
-  app = express();
+const express = require('express');
+const Sqlite = require('better-sqlite3');
+// const path = require('path');
+const routes = require('./routes');
+
+const app = express();
 
 const port = process.env.PORT || 3001;
 const picDB = process.env.picDB || 'pictures.db';
 
-const db = new sqlite(picDB);
-db.exec("CREATE TABLE IF NOT EXISTS pic (id TEXT, date NUMBER, thumbnail BLOB, mime TEXT, size TEXT, filename TEXT, hasExif NUMBER)");
+const db = new Sqlite(picDB);
+db.exec('CREATE TABLE IF NOT EXISTS pic (id TEXT, date NUMBER, thumbnail BLOB, mime TEXT, size TEXT, filename TEXT, hasExif NUMBER)');
 
-app.set("db", db);
+app.set('db', db);
 app.use('/', express.static('./'));
 app.use('/api', routes);
 app.listen(port, () => {
-  console.log("API-Server auf Port ", port);
+  console.log('API-Server auf Port ', port);
 });
 
-module.exports = app;  // for mocha/chai tests
+module.exports = app; // for mocha/chai tests
