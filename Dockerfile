ddc2c2e29172aee77c8f712f94aa6d121d533bdb
Bernd Oeggl <bernd@webman.at>
2025-05-15 09:46:53 +0200

    updates 2025

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:16-alpine
+FROM node:24
 WORKDIR /src
 COPY package*.json /src/
-RUN npm i --production
+RUN npm i --omit=dev
 COPY server.js routes.js index.html /src/
-CMD npm start
+CMD ["npm", "start"]
--- routes.js
+++ routes.js
@@ -1,45 +1,59 @@
-const Jimp = require('jimp'),
+const { Jimp } = require("jimp"),
   express = require("express"),
-  multer = require('multer'),
+  multer = require("multer"),
   upload = multer(),
   debug = require("debug")("api"),
-  eparser = require('exif-parser'),
+  eparser = require("exif-parser"),
   router = express.Router(),
-  uuid = require('uuid');
+  uuid = require("uuid");
 
 router.get("/pictures", (req, res) => {
   const rows = req.app.get("db").prepare("SELECT * from pic").all();
-  res.json({data: rows});
+  res.json({ data: rows });
 });
-router.post("/picture", upload.single('file'), (req, res) => {
+router.post("/picture", upload.single("file"), async (req, res) => {
   debug("Files uploaded: ", req.file);
   let origDate = Date.now();
   let hasExif = 0;
-  if (req.file.mimetype === 'image/jpeg') {
+  if (req.file.mimetype === "image/jpeg") {
     const parser = eparser.create(req.file.buffer);
     const exif = parser.parse();
-    debug('EXIF is: ', exif);
+    debug("EXIF is: ", exif);
     if (exif.tags && exif.tags.DateTimeOriginal) {
       hasExif = 1;
       origDate = exif.tags.DateTimeOriginal;
-      debug('exifdate: ', origDate);
+      debug("exifdate: ", origDate);
     }
   }
-  Jimp.read(req.file.buffer).then(function (image) {
-    image.cover(200, 200)
-      .getBuffer(req.file.mimetype, function (err, buff) {
-        const entry = [ uuid.v1(), origDate, Buffer.from(buff, 'base64'), req.file.mimetype, req.file.size, hasExif ];
-        const query = "INSERT INTO pic (id, date, thumbnail, mime, size, hasExif) VALUES (?,?,?,?,?,?)";
-        const data = req.app.get("db").prepare(query).run(entry);
-        res.json({ data: data});
-      })
-  })
+
+  debug("buffer is: ", req.file);
+  const image = await Jimp.fromBuffer(req.file.buffer);
+  // debug("image is: ", image);
+  // .then(function (image) {
+  const cimg = image.cover({ w: 200, h: 200 });
+  const buff = await cimg.getBuffer(req.file.mimetype); // , function (err, buff) {
+  const entry = [
+    uuid.v1(),
+    origDate,
+    Buffer.from(buff, "base64"),
+    req.file.mimetype,
+    req.file.size,
+    hasExif,
+  ];
+  const query =
+    "INSERT INTO pic (id, date, thumbnail, mime, size, hasExif) VALUES (?,?,?,?,?,?)";
+  const data = req.app.get("db").prepare(query).run(entry);
+  res.json({ data: data });
+  // });
+  // });
 });
 router.delete("/picture/:id", (req, res) => {
-  debug('delete for ', req.params.id);
-  const data = req.app.get("db").prepare("DELETE FROM pic WHERE id = ?").run([req.params.id]);
-  res.json({data: data});
+  debug("delete for ", req.params.id);
+  const data = req.app
+    .get("db")
+    .prepare("DELETE FROM pic WHERE id = ?")
+    .run([req.params.id]);
+  res.json({ data: data });
 });
-
 
 module.exports = router;
